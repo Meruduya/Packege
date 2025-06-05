8e97f230af0fd9d38769608c5756e9e3aac4c8d9
Veronika <korynaitroyd@gmail.com>
2025-06-05 11:21:29 +0300

    ParameterixesTest

--- src/test/java/ru/netology/services/FreelancerServiceTest.java
+++ src/test/java/ru/netology/services/FreelancerServiceTest.java
@@ -2,6 +2,8 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +18,7 @@ class FreelancerServiceTest {
         int expenses = 3_000;
         int threshold = 20_000;
 
-         // Act - шаги теста
+        // Act - шаги теста
         int actual = service.calculate(income, expenses, threshold);
 
         // Assert - проверки данных
@@ -27,19 +29,24 @@ class FreelancerServiceTest {
     @Test
     public void testCalculateRestMonth2() {
 
-        // AAA
-        // Arrange - подготовка к тесту
         FreelancerService service = new FreelancerService();
         int income = 100_000;
         int expenses = 60_000;
         int threshold = 150_000;
 
-        // Act - шаги теста
         int actual = service.calculate(income, expenses, threshold);
 
-        // Assert - проверки данных
         int expected = 2;
         Assertions.assertEquals(expected, actual);
     }
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "test_data.csv")
+    public void testWithParams(int income, int expenses, int threshold, int expected) {
+        FreelancerService service = new FreelancerService();
+
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
